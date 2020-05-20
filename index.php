c38b501cbd6dd3969f135366406c774e8e76e5f8
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-05-20 15:47:33 +0200

    Project files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    
+    <h1>vaffanculo npm </h1>
+
+    <!-- script -->
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js'></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
